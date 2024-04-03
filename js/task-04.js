ae2d2c672aa4c24b663420b6c9eb778c7a230aa9
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-04-03 10:23:36 +0200

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,11 @@
+let counterValue = 0;
+const btnPlus = document.querySelector('button[data-action="increment"]');
+const btnMinus = document.querySelector('button[data-action="decrement"]');
+const value = document.querySelector("#value");
+
+increase = () => {
+  counterValue += 1;
+  value.value = counterValue;
+};
+
+btnPlus.addEventListener("click", increase);
