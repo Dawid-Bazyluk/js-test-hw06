935e8e7b55eeec2250b27a3c2cb440c310f2907e
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-04-03 13:40:49 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const email = loginForm.elements.email.value;
+  const password = loginForm.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const dataForm = {
+    email: email,
+    password: password,
+  };
+  console.log(dataForm);
+  loginForm.reset();
+}
